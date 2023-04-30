1dccb01e90d9dea5e4f8c17bf4ed39c14953a018
Supersubzero <sweet5433@outlook.com>
2023-04-30 15:54:57 +0530

    quick fix

--- resume_app/views.py
+++ resume_app/views.py
@@ -9,6 +9,7 @@ from rest_framework import status
 from django.http import JsonResponse,HttpResponse
 from django.template import loader
 import pdfkit
+import json
 
 
 
@@ -17,42 +18,49 @@ def candidate_list(request):
     if request.method=="GET":
         paginator = PageNumberPagination()  #intiating pagination
         paginator.page_size = 10
-        city = request.query_params.get('city', None) #getting querries from end point
-        tech_skills = request.query_params.get('tech_skills', None)
+        city1 = request.query_params.get('city', None) #getting querries from end point
+        tech_skills1 = request.query_params.get('tech_skills', None)
 
         candidates = Candidate.objects.all()
-        if city:
-            candidates = candidates.filter(city=city) #set queeryset to fetch city filter
-        if tech_skills:
-            candidates = candidates.filter(tech_skills__icontains=tech_skills) #set querryset to fetch tech_skill filter
+        if city1:
+            
+            candidates= candidates.filter(city__icontains=city1) #set queeryset to fetch city filter
+            result_page = paginator.paginate_queryset(candidates, request)
+            serializer = CandidateSerializer(result_page, many=True)
+            return paginator.get_paginated_response(serializer.data)
+        if tech_skills1:
+            candidates = candidates.filter(tech_skills__icontains=tech_skills1) #set querryset to fetch tech_skill filter
+            result_page = paginator.paginate_queryset(candidates, request)
+            serializer = CandidateSerializer(result_page, many=True)
+            return paginator.get_paginated_response(serializer.data)       
         else:
             candidates = Candidate.objects.all()[0:50] #set querryset to fetch 50 records
-        result_page = paginator.paginate_queryset(candidates, request)
-        serializer = CandidateSerializer(result_page, many=True)
-        return paginator.get_paginated_response(serializer.data)
+            result_page = paginator.paginate_queryset(candidates, request)
+            serializer = CandidateSerializer(result_page, many=True)
+            return paginator.get_paginated_response(serializer.data)
         # Check if request is a POST request
     if request.method == 'POST':
         # Check if data is valid
         
         data = request.data
-        if not data:
-            return Response(status=status.HTTP_204_NO_CONTENT)
+        ser = CandidateSerializer(data=data)
+        if ser.is_valid():
 
-        # If single record, create and save
-        if isinstance(data, dict):
-            candidate = Candidate(**data)
-            candidate.save()
-            return Response(status=status.HTTP_201_CREATED)
+            # If single record, create and save
+            if isinstance(data, dict):
+                candidate = Candidate(**data)
+                candidate.save()
+                return Response(status=status.HTTP_201_CREATED)
+            
+            # If multiple records, create and save each one
+            if isinstance(data, list):
+                candidates = []
+                for record in data:
+                    candidate = Candidate(**record)
+                    candidates.append(candidate)
+                Candidate.objects.bulk_create(candidates)
+                return Response({'success': f'{len(candidates)} candidate records created'})
         
-        # If multiple records, create and save each one
-        if isinstance(data, list):
-            candidates = []
-            for record in data:
-                candidate = Candidate(**record)
-                candidates.append(candidate)
-            Candidate.objects.bulk_create(candidates)
-            return Response({'success': f'{len(candidates)} candidate records created'})
-    
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
